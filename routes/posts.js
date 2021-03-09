ea70545c383d41fd65ab7213026a470aeb4cd648
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-03-09 15:51:14 -0400

    Added get and delete post routes and pugs for testing

--- routes/posts.js
+++ routes/posts.js
@@ -3,13 +3,26 @@ const db = require("../db/models");
 const router = express.Router();
 const { csrfProtection, asyncHandler } = require("./utils");
 const { check, validationResult } = require("express-validator");
-const post = require("../db/models/post");
 const { loginUser, logoutUser } = require("../auth");
 
-router.get('/posts/:id(\\d+)', asyncHandler( async ( req, res) => {
-  const postId = parseInt(req.params.id, 10)
-  const post = await db.Post.findByPk(postId) // come back and include comments table
-  res.render('post-detail', {title: post.title, post})
-}))
+router.get(
+  "/posts/:id(\\d+)",
+  asyncHandler(async (req, res) => {
+    const postId = parseInt(req.params.id, 10);
+    const post = await db.Post.findByPk(postId); // come back and include comments table
+    res.render("post-detail", { title: post.title, post });
+  })
+);
 
-module.exports = router
+router.post(
+  "/posts/:id(\\d+)",
+  csrfProtection,
+  asyncHandler(async (req, res) => {
+    const postId = parseInt(req.params.id, 10);
+    const post = await db.Post.findByPk(postId);
+    await post.destroy();
+    res.redirect("/");
+  })
+);
+
+module.exports = router;
