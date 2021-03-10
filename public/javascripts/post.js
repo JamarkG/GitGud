36e84535734bceadec73d407c54a15895600d8d9
jairo <jairo_cal@yahoo.com>
2021-03-10 14:48:27 -0800

    working on creation of the post

--- public/javascripts/post.js
+++ public/javascripts/post.js
@@ -6,10 +6,46 @@ deleteButtons.forEach((button) =>
     const result = await fetch(`/comments/${commentId}`, {
       method: "DELETE",
     });
-    console.log(commentId);
     const data = await result.json();
     if ((data.success = "Success!")) {
       e.target.parentElement.remove();
     }
   })
 );
+
+// const editButtons = document.querySelectorAll(".edit-button");
+
+// editButtons.forEach((button) =>
+// 	button.addEventListener("click", async (e) => {
+// 		const commentId = e.target.id;
+// 		const result = await fetch(`/comments/${commentId}`, {
+// 			method: "DELETE",
+// 		});
+// 		const data = await result.json();
+// 		if ((data.success = "Success!")) {
+// 			e.target.parentElement.remove();
+// 		}
+// 	})
+// );
+
+const addButtons = document.querySelectorAll(".add-button");
+
+addButtons.forEach((button) =>
+	button.addEventListener("click", async (e) => {
+    const comment = document.querySelector(".new-comment-box");
+    console.log(comment.name)
+		const result = await fetch(`/posts/${comment.name}/comments`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ body: comment.value }), // send in the text from the input box
+		});
+		const data = await result.json();
+		if ((data.success = "Success!")) {
+      console.log("We made it here now show the comment")
+		}
+	})
+);
+
+// above we just need to make it so that it is rendered to the page right away
--- routes/comments.js
+++ routes/comments.js
@@ -5,6 +5,7 @@ const { csrfProtection, asyncHandler } = require("./utils");
 const { check, validationResult } = require("express-validator");
 const { loginUser, logoutUser, requireAuth } = require("../auth");
 
+// route to delete a comment
 router.delete(
   "/comments/:id(\\d+)",
   requireAuth,
@@ -15,5 +16,19 @@ router.delete(
     res.json({ success: "Success!" });
   })
 );
+
+// make it so that when this route is hit then we create a comment with a postId for the page that we are on
+// and the userId for the person in the current session
+router.post("/posts/:id/comments", requireAuth, asyncHandler(async ( req, res) => {
+  console.log(req.body)
+  const postId = parseInt(req.params.id, 10)
+  const { userId } = req.session.auth;
+  const comment = await db.Comment.create({
+    body: req.body.comment,
+    postId,
+    userId,
+  })
+  res.json({ success: "Success!" });
+}))
 
 module.exports = router;
