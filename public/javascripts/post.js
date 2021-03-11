2e6496731f1bea52eb7599e2e294caa5d2a5dbe0
Alex Rodriguez <alex.rodriguezsntg@gmail.com>
2021-03-11 12:13:55 -0400

    testing comment edits with hidden css

--- public/javascripts/post.js
+++ public/javascripts/post.js
@@ -28,42 +28,71 @@ addButton.addEventListener("click", async (e) => {
   if ((data.success = "Success!")) {
     const comment = data.comment;
     const commentsContainer = document.querySelector(".comments-container");
-    const commentBox = document.createElement("p");
-    commentBox.innerHTML = `<div class="comment-box-${comment.id}">
-          <div class="comment-box-body" id= "comment-${comment.id}" >
+    // const commentBox = document.createElement("div");
+    // commentsContainer.appendChild(commentBox);
+    commentsContainer.innerHTML += `
+      <div class="comment-box-${comment.id}">
+        <div class="comment-box-body" id= "comment-${comment.id}" >
             <p class="comment-box-text" id="text-${comment.id}">${commentText}</p>
-          </div>
-          <button class="delete-button">Delete</button>
-          <button class="edit-button">Edit</button>
-       </div>
+        </div>
+        <button class="delete-button">Delete</button>
+        <button class="edit-button">Edit</button>
+        <div class="edit-box__hidden">
+          <textarea>
+           ${comment}
+          </textarea>
+          <button class="update-button" id="update-${comment.id}">
+            Update
+          </button>
+          <button class="cancel-button" id="cancel-${comment.id}">
+            Cancel
+          </button>
+        </div>
+      </div>
         `;
-    commentsContainer.appendChild(commentBox);
-    comment.value = "";
+    // commentsContainer.appendChild(commentBox);
+    // comment.value = "";
+
+    document.querySelector(".new-comment-box").value = "";
   }
 });
 
 const editButtons = document.querySelectorAll(".edit-button");
 
 editButtons.forEach((button) => {
-  button.addEventListener("click", async (e) => {
+  button.addEventListener("click", (e) => {
     const commentId = e.target.id;
     const comment = document.getElementById(`text-${commentId}`).innerText;
-    const commentBox = document.querySelector(`.comment-box-${commentId}`);
+    let commentBox = document.querySelector(`.comment-box-${commentId}`);
     const originalCommentBox = commentBox;
     console.log(comment);
 
     // const textArea = document.createElement("textarea");
     // textArea.innerText = comment.value;
-    commentBox.innerHTML = `
-    <textarea>
-        ${comment}
-    </textarea>
-    <button class="update-button" id="update-${commentId}">
-        Update
-    </button>
-    <button class="cancel-button id="cancel-${commentId}">
-        Cancel
-    </button>`;
+    // commentBox.innerHTML += `
+    // <div class="edit-box__hidden">
+    // <textarea>
+    //     ${comment}
+    // </textarea>
+    // <button class="update-button" id="update-${commentId}">
+    //     Update
+    // </button>
+    // <button class="cancel-button" id="cancel-${commentId}">
+    //     Cancel
+    // </button>
+    // <div>`;
+
+    document
+      .querySelector(".edit-box__hidden")
+      .classList.remove("edit-box__hidden");
+  });
+});
+
+const cancelButtons = document.querySelectorAll(".cancel-button");
+
+cancelButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.target.parentElement.classList.add("edit-box__hidden");
   });
 });
 
