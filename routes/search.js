6f0f9b8de8408d774151f6ba75ca094aa20325fa
Mark Gregory <markgregory19@gmail.com>
2021-03-12 15:05:56 -0800

    working on search display functionality

--- routes/search.js
+++ routes/search.js
@@ -1,17 +1,36 @@
 const express = require("express");
 const db = require("../db/models");
 const router = express.Router();
+// const { sequelize } = require('../app.js');
+// const { Op } = require("sequelize");
 const { csrfProtection, asyncHandler } = require("./utils");
-const { check, validationResult } = require("express-validator");
-const { loginUser, logoutUser, requireAuth } = require("../auth");
+// const { check, validationResult } = require("express-validator");
+// const { loginUser, logoutUser, requireAuth } = require("../auth");
 
-router.get('/search', asyncHandler(async (req, res) => {
-    console.log(req.query);
-    // const postsTopics = await db.PostTopic.findAll({});
+router.get('/search', csrfProtection, asyncHandler(async (req, res) => {
+    // console.log(req.query.search);
+    const topic = await db.Topic.findOne({
+        where: {
+            name: `${req.query.search}`
+        }
+    })
+    const topicID = topic.dataValues.id
+    // console.log(typeof topicID)
+    if (topic){
+        const postIDs = await db.PostTopic.findAll({ where:
+            { topicId: topicID
+        }})
+        console.log(postIDs.json())
+    }
+
+    // console.log(topic);
+    // const topicName = topic.dataValues.name;
+    // const topicID = topic.id
+    // const postsTopics = await db.PostTopic.findAll({ where: topicID =  });
     // const topics = postsTopic.topicId
     // const posts = await db.Post.findAll({ include: topic});
-    // res.render('search', posts)
-}))
+    res.render('search', { topic, csrfToken: req.csrfToken() });
+}));
 
 
 
