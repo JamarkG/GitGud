d9afefa7a0824a26c47f947ca7d17b97837ade30
jairo <jairo_cal@yahoo.com>
2021-03-14 15:17:12 -0700

    added searchbar CSS

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,10 @@
 window.addEventListener("load", (event) => {
   console.log("hello from javascript!");
 });
+
+// let progress = document.getElementById("progressbar");
+// let totalHeight = document.body.scrollHeight - window.innerHeight;
+// window.onscroll = function() {
+//   let progressHeight = (window.pageYOffset / totalHeight) * 100;
+//   progress.style.height = progressHeight + "%"
+// }
